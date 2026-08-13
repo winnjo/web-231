64f8ade4639aacd75bf3ebc51a0cac2e4910baf9
Winston Jo <122560578+winstonjo@users.noreply.github.com>
2026-08-13 08:21:16 -0600

    week 1

---
+++ week-1/project01-02/project01-02_txt.js
@@ -0,0 +1,19 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Winston Jo
+    Date:   8/13/2026
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+const service1Name = 'Basic'
+const service2Name = 'Express'
+const service3Name = 'Extreme'
+const service4Name = 'Ultimate'
+const service1Speed = '0 Mbps'
+const service2Speed = '100 Mbps'
+const service3Speed = '500 Mbps'
+const service4Speed = '1 Gig'
